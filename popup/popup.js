7f8cb759657a927fad00897393ee9f875295a857
Rodrigo Jimenez <rjc.2002vmx@gmail.com>
2023-06-17 18:14:37 -0600

    initial commit

---
+++ popup/popup.js
@@ -0,0 +1,64 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginPage = document.getElementById('login-page');
+    const homePage = document.getElementById('home-page');
+    const section1Button = document.getElementById('section1-button');
+    const section2Button = document.getElementById('section2-button');
+    const section3Button = document.getElementById('section3-button');
+    const section1 = document.getElementById('section1');
+    const section2 = document.getElementById('section2');
+    const section3 = document.getElementById('section3');
+    const navBar = document.getElementById('nav-bar');
+
+    function showHomePage() {
+        loginPage.style.display = 'none';
+        homePage.style.display = 'block';
+        navBar.style.display = 'block';
+    }
+
+    function showSection1() {
+        section3.style.display = 'none';
+        section2.style.display = 'none';
+        section1.style.display = 'block';
+    }
+
+    function showSection2() {
+        section1.style.display = 'none';
+        section3.style.display = 'none';
+        section2.style.display = 'block';
+    }
+
+    function showSection3() {
+        section1.style.display = 'none';
+        section2.style.display = 'none';
+        section3.style.display = 'block';
+    }
+
+    const loginButton = document.getElementById('login-button');
+
+    // async function getUserInfo() {
+    //     const response = await fetch('https://extensionpracticefunc.azurewebsites.net/.auth/me')
+            
+    //     const payload = await response.json();
+    //     const { clientPrincipal } = payload;
+    //     return clientPrincipal;
+    // }
+
+    loginButton.addEventListener('click', async function() {
+        // console.log(await getUserInfo())
+        console.log("Hola, si srive el boton :D")
+
+        showHomePage();
+    });
+
+    section1Button.addEventListener('click', function() {
+        showSection1();
+    });
+
+    section2Button.addEventListener('click', function() {
+        showSection2();
+    });
+
+    section3Button.addEventListener('click', function() {
+        showSection3();
+    });
+});
